11e5515234805b141034de88d6bd1414dcfcf3f4
sarvessen <92943595+sarvessen@users.noreply.github.com>
2022-03-24 16:41:58 +0400

    Add files via upload

---
+++ design.js
@@ -0,0 +1,27 @@
+function change1()
+ {
+   document.getElementById('pic').src='1.png';
+
+}
+function change2()
+ {
+   document.getElementById('pic').src='2.png';
+
+}
+function change3()
+ {
+   document.getElementById('pic').src='3.png';
+
+}
+
+function change4()
+ {
+   document.getElementById('pic').src='4.png';
+
+}
+
+function change5()
+ {
+   document.getElementById('pic').src='5.png';
+
+}
